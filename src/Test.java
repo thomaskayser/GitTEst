f29db351f1bfe6b989b72bdd8d1ea5820269049b
tom <thomas.kayser@anea.ch>
2015-06-19 09:59:15 +0200

    korrekt package

--- src/Test.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Tom on 19.06.15.
- */
-public class Test {
-    private static final String VERSION = "Test V1";
-}
---
+++ src/ch/tkayser/gitignore/Test.java
@@ -0,0 +1,8 @@
+package ch.tkayser.gitignore;
+
+/**
+ * Created by Tom on 19.06.15.
+ */
+public class Test {
+    private static final String VERSION = "ch.tkayser.gitignore.Test V1";
+}
