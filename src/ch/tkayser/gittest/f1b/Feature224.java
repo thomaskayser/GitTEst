d6876fd794628dd35521a6a2a2d16ab73fa41982
tom <thomas.kayser@anea.ch>
2015-06-19 20:08:09 +0200

    package names fixed

---
+++ src/ch/tkayser/gittest/f1b/Feature224.java
@@ -0,0 +1,29 @@
+package ch.tkayser.gittest.f1b;
+
+
+public class Feature224 {
+
+
+    private String p1;
+    private String p2;
+
+    public Feature224() {
+
+    }
+
+    public String getP1() {
+        return p1;
+    }
+
+    public void setP1(String p1) {
+        this.p1 = p1;
+    }
+
+
+    public void showMeTheInf() {
+        System.out.println("Feature "+getClass().getName());
+    }
+
+
+}
+
---
+++ src/ch/tkayser/gittest/f1c/Feature2682.java
@@ -0,0 +1,29 @@
+package ch.tkayser.gittest.f1c;
+
+
+public class Feature2682 {
+
+
+    private String p1;
+    private String p2;
+
+    public Feature2682() {
+
+    }
+
+    public String getP1() {
+        return p1;
+    }
+
+    public void setP1(String p1) {
+        this.p1 = p1;
+    }
+
+
+    public void showMeTheInf() {
+        System.out.println("Feature "+getClass().getName());
+    }
+
+
+}
+
