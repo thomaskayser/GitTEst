da2f82af224af20cbd75c4c8d17d697cace68694
tom <thomas.kayser@anea.ch>
2015-06-19 10:02:36 +0200

    version 1 implemented (fix)

--- src/ch/tkayser/gitignore/Test.java
+++ src/ch/tkayser/gitignore/Test.java
@@ -7,6 +7,6 @@ public class Test {
     private static final String VERSION = "ch.tkayser.gitignore.Test V1";
 
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("Test in Version: "+VERSION);
     }
 }
