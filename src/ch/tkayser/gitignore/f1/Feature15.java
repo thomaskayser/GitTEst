bfc30dc37ce6e9086aea6c35f3ee5a26ba41997f
tom <thomas.kayser@anea.ch>
2015-06-19 14:03:35 +0200

    feature 1

---
+++ src/ch/tkayser/gitignore/f1/Feature15.java
@@ -0,0 +1,29 @@
+package ch.tkayser.gitignore.f1;
+
+
+public class Feature15 {
+
+
+    private String p1;
+    private String p2;
+
+    public Feature15() {
+
+    }
+
+    public String getP1() {
+        return p1;
+    }
+
+    public void setP1(String p1) {
+        this.p1 = p1;
+    }
+
+
+    public void showMeTheInf() {
+        System.out.println("Feature "+getClass().getName());
+    }
+
+
+}
+
