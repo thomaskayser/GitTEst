4ca062a65d38d9c527d966bd50a130265569086f
tom <thomas.kayser@anea.ch>
2015-06-19 13:57:51 +0200

    generator finished

--- src/ch/tkayser/gitignore/Generator.java
+++ src/ch/tkayser/gitignore/Generator.java
@@ -1,8 +1,11 @@
 package ch.tkayser.gitignore;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.StringTokenizer;
 
 /**
  * Created by Tom on 19.06.2015.
@@ -16,15 +19,39 @@ public class Generator {
         }
 
         String outputDir = args[0];
-        String pckgName = args[1];
+        String pkgName = args[1];
         Integer features = Integer.parseInt(args[2]);
+
+        createOutputDir(outputDir, pkgName);
 
         URI filePath = Generator.class.getClassLoader().getResource("pattern.txt").toURI();
         for (int i = 0; i < features; i++) {
+            String featureNr = String.valueOf(i+1);
             String text = new String(Files.readAllBytes(Paths.get(filePath)));
-            text = text.replace("<<PACKAGE>>", pckgName);
-            text = text.replace("<<FEATURE>>", String.valueOf(i+1));
-            System.out.println(text);
+            text = text.replace("<<PACKAGE>>", pkgName);
+            text = text.replace("<<FEATURE>>", featureNr);
+
+            File packageDir = new File(outputDir+"/"+pkgName.replaceAll("\\.","/")+"/Feature"+featureNr+".java");
+            FileWriter writer = new FileWriter(packageDir);
+            writer.write(text);
+            writer.close();
+        }
+
+
+
+    }
+
+    private static void createOutputDir(String outputDir, String packageName) {
+        File root = new File(outputDir);
+        root.mkdir();
+        StringTokenizer tokens = new StringTokenizer(packageName, ".");
+
+        File currentDir = root;
+        while (tokens.hasMoreElements()) {
+            String folder = tokens.nextToken();
+            File packageDir = new File(currentDir, folder);
+            packageDir.mkdir();
+            currentDir = packageDir;
         }
 
 
