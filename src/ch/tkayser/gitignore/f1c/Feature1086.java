3e26f827476296b484a1e312c5e9af42570a7b6a
tom <thomas.kayser@anea.ch>
2015-06-19 14:32:21 +0200

    package name fixed

---
+++ src/ch/tkayser/gitignore/f1c/Feature1086.java
@@ -0,0 +1,29 @@
+package ch.tkayser.gitignore.f1c;
+
+
+public class Feature1086 {
+
+
+    private String p1;
+    private String p2;
+
+    public Feature1086() {
+
+    }
+
+    public String getP1() {
+        return p1;
+    }
+
+    public void setP1(String p1) {
+        this.p1 = p1;
+    }
+
+
+    public void showMeTheInf() {
+        System.out.println("Feature "+getClass().getName());
+    }
+
+
+}
+
---
+++ src/ch/tkayser/gitignore/f1c/Feature4244.java
@@ -0,0 +1,29 @@
+package ch.tkayser.gitignore.f1c;
+
+
+public class Feature4244 {
+
+
+    private String p1;
+    private String p2;
+
+    public Feature4244() {
+
+    }
+
+    public String getP1() {
+        return p1;
+    }
+
+    public void setP1(String p1) {
+        this.p1 = p1;
+    }
+
+
+    public void showMeTheInf() {
+        System.out.println("Feature "+getClass().getName());
+    }
+
+
+}
+
