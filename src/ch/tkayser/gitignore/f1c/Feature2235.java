73cd89f49438dc7f06ca032f0e04c59919bf242e
tom <thomas.kayser@anea.ch>
2015-06-19 14:32:21 +0200

    package name fixed

---
+++ src/ch/tkayser/gitignore/f1c/Feature2235.java
@@ -0,0 +1,29 @@
+package ch.tkayser.gitignore.f1c;
+
+
+public class Feature2235 {
+
+
+    private String p1;
+    private String p2;
+
+    public Feature2235() {
+
+    }
+
+    public String getP1() {
+        return p1;
+    }
+
+    public void setP1(String p1) {
+        this.p1 = p1;
+    }
+
+
+    public void showMeTheInf() {
+        System.out.println("Feature "+getClass().getName());
+    }
+
+
+}
+
---
+++ src/ch/tkayser/gitignore/f1c/Feature3794.java
@@ -0,0 +1,29 @@
+package ch.tkayser.gitignore.f1c;
+
+
+public class Feature3794 {
+
+
+    private String p1;
+    private String p2;
+
+    public Feature3794() {
+
+    }
+
+    public String getP1() {
+        return p1;
+    }
+
+    public void setP1(String p1) {
+        this.p1 = p1;
+    }
+
+
+    public void showMeTheInf() {
+        System.out.println("Feature "+getClass().getName());
+    }
+
+
+}
+
