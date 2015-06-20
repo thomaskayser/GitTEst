5d74e8ef9a303bd884c54319617f15ba3b5ae6fb
tom <thomas.kayser@anea.ch>
2015-06-20 12:05:22 +0200

    U1: V1.2

--- src/ch/tkayser/gittest/Test.java
+++ src/ch/tkayser/gittest/Test.java
@@ -4,7 +4,7 @@ package ch.tkayser.gittest;
  * Created by Tom on 19.06.15.
  */
 public class Test {
-    private static final String VERSION = "ch.tkayser.gittest.Test V1.1";
+    private static final String VERSION = "ch.tkayser.gittest.Test V1.2";
 
     public static void main(String[] args) {
         System.out.println("Test in Version: "+VERSION);
